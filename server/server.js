87ee6df3399cc7d1b705cba6029a59b675c4f55f
Nguyễn Thế Hùng <vt2804@gmail.com>
2019-06-05 06:51:12 +0700

    fff

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ app.use(function(req, res, next) {
 app.use(cookieParser());
 
 
-//app.use(express.static(path.resolve(__dirname,'../client','build')));
-app.use(express.static('client/build'));
+app.use(express.static(path.resolve(__dirname,'../client','build')));
+//app.use(express.static('client/build'));
 var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
