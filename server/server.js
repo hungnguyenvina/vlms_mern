cbb8bcf75485eb7d00b56494c1b8876744ae3643
Nguyễn Thế Hùng <vt2804@gmail.com>
2019-06-04 23:13:27 +0700

    serverjsfixf

--- server/server.js
+++ server/server.js
@@ -34,8 +34,7 @@ app.use(function(req, res, next) {
 app.use(cookieParser());
 
 
-//app.use(express.static(path.resolve(__dirname,'../client','build')));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname,'../client','build')));
 
 var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
